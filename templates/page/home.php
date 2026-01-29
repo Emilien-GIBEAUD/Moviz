152dd3b71789f74ad6dd50c0390e8a0daca1cf2d
Emilien-GIBEAUD <emiliengibeaud@gmail.com>
2026-01-29 12:43:30 +0100

    Minor update (v0.1.1) on the home page to validate the deployment procedure.

--- templates/page/home.php
+++ templates/page/home.php
@@ -2,7 +2,7 @@
 require_once _ROOTPATH_ . '/templates/header.php'; 
 ?>
 
-<h1>Bienvenue sur la page d'accueil de Moviz</h1>
-<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. In porro, ut autem dolores facere consequatur doloribus eius odit accusantium sequi provident nostrum ex quibusdam nisi perferendis, veritatis aperiam mollitia, voluptatem numquam! Obcaecati qui architecto debitis dolorum aspernatur facere perferendis, provident labore dicta vero ipsa, eius est, eos perspiciatis quisquam aut.</p>
+<h1>Bienvenue sur Moviz</h1>
+<p>Moviz est une simple application de critique de films.</p>
 
 <?php require_once _ROOTPATH_ . '/templates/footer.php'; ?>
